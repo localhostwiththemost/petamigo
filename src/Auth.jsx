dbe034827b0aaf9e9edd2d0bd21ab76f5d8e3790
localhostwiththemost <jpowelldev@gmail.com>
2023-04-12 15:48:40 -0500

    Added styles to Google Auth page and created favicon

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -1,9 +1,7 @@
-
 import { auth, googleProvider } from "./firebase";
 import { signInWithPopup, signOut } from "firebase/auth";
 
 export const Auth = () => {
-
   const signInWithGoogle = async () => {
     try {
       await signInWithPopup(auth, googleProvider);
@@ -12,20 +10,29 @@ export const Auth = () => {
     }
   };
 
-  const logOut = async () => {
-    try {
-      await signOut(auth);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  
-
+  // const logOut = async () => {
+  //   try {
+  //     await signOut(auth);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
 
   return (
     <>
-      <button onClick={signInWithGoogle}>Sign In With Google</button>
-      <button onClick={logOut}>Sign Out</button>
+      <section className="auth-section">
+        <p className="auth-text">To submit a pet for adoption, please sign in using your Google account.</p>
+        
+        <button className="google-btn" onClick={signInWithGoogle}>
+          <img
+            className="google-icon"
+            src="src/images/google-icon.webp"
+            alt="Google logo"
+          />{" "}
+          Sign In With Google
+        </button>
+      </section>
+      {/* <button onClick={logOut}>Sign Out</button> */}
     </>
   );
 };
